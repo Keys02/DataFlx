f260b42350ccdf9686dc715916f0bfb5f619165e
Keys <keys14052002@gmail.com>
2025-07-04 23:47:17 +0000

    Set up searching and sorting operations controller

--- src/main/java/org/dataflx/Application.java
+++ src/main/java/org/dataflx/Application.java
@@ -1,7 +1,17 @@
 package org.dataflx;
+import java.util.Scanner;
 
 public class Application {
     public void run () {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What operation would you like to perform?");
+        System.out.println("Press 1 - Searching");
+        System.out.println("Press 2 - Sorting");
+        int operation = scanner.nextInt();
+
+        if (operation == 1) {
+
+        }
+
     }
 }
---
+++ src/main/java/org/dataflx/ApplicationController.java
@@ -0,0 +1,14 @@
+package org.dataflx;
+import java.util.*;
+
+public class ApplicationController
+{
+    // Searching operations controller
+    public double performSearch(List<Double> arr, double number) {
+
+    }
+    // Sorting operations controller
+    public double performSort(List<Double> arr, double number) {
+
+    }
+}
