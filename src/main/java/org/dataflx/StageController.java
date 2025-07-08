ad6d87f7dee10e47f8a52ba3d6ee6755f630971f
Keys <keys14052002@gmail.com>
2025-07-08 02:23:44 +0000

    Make the parameter names of the searchOperationResult and sortOperationResult methods more clear

--- src/main/java/org/dataflx/StageController.java
+++ src/main/java/org/dataflx/StageController.java
@@ -98,16 +98,16 @@ public class StageController
         System.out.println(ANSI.RED + "⚠️: Incorrect choice, try again" + ANSI.RESET);
     }
 
-    public void searchOperationResult(int searchedNumIndex, String notice, String runtime) {
+    public void searchOperationResult(int searchedNumIndex, String notice, String runtimeInBigONotation) {
         System.out.println("Algorithm: " + ANSI.BLUE  + ANSI.BOLD + notice + ANSI.RESET);
         System.out.println("The number can be found at index " + ANSI.BOLD + ANSI.PURPLE + searchedNumIndex + ANSI.RESET);
-        System.out.println("Runtime in Big O Notation: " + ANSI.BOLD + ANSI.YELLOW + runtime + ANSI.RESET + " in the worse case scenario.");
+        System.out.println("Runtime in Big O Notation: " + ANSI.BOLD + ANSI.YELLOW + runtimeInBigONotation + ANSI.RESET + " in the worse case scenario.");
     }
 
-    public void sortOperationResult(String arr, String notice, String runtime) {
+    public void sortOperationResult(String arr, String notice, String runtimeInBigONotation) {
         System.out.println("Algorithm: " + ANSI.BLUE  + ANSI.BOLD + notice + ANSI.RESET);
         System.out.println("Sorted Array: " + ANSI.PURPLE + arr + ANSI.RESET);
-        System.out.println("Runtime in Big O Notation: " + ANSI.BOLD + ANSI.YELLOW + runtime + ANSI.RESET + " in the worse case scenario.");
+        System.out.println("Runtime in Big O Notation: " + ANSI.BOLD + ANSI.YELLOW + runtimeInBigONotation + ANSI.RESET + " in the worse case scenario.");
     }
 
 }
